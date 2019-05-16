c7cbe1814aee6e90ad9c5d95bcd4856a7ddad899
Vladimir Danilov <vldanilov90@gmail.com>
2019-05-16 20:37:25 +0300

    Extra two approaches has added

--- VHDL/Basics/std_mux1/std_mux1.vhd
+++ VHDL/Basics/std_mux1/std_mux1.vhd
@@ -21,9 +21,21 @@ architecture std_nux1_arch of std_mux1 is
 	signal in_3_bus : std_logic_vector(3 downto 0);
 
 begin
-
-	in_3_bus <= (in_3 & in_3 & in_3 & in_3);
+	--------------------------------------------------------------------
+	-- FIRST approach
+	-- in_3_bus <= (in_3 & in_3 & in_3 & in_3);
 	
-	out_1 <= ((NOT in_3_bus) AND in_1) OR (in_3_bus AND in_2);
+	-- out_1 <= ((NOT in_3_bus) AND in_1) OR (in_3_bus AND in_2);
+	--------------------------------------------------------------------
+	-- SECOND approach
+	-- with (in_3) select
+		-- out_1 <= 	in_1 when '0',
+					-- in_2 when '1',
+					-- "0000" when others;
+	--------------------------------------------------------------------			
+	-- THIRD approach
+	out_1 <= 	in_1 when in_3 <= '0' else
+				in_2 when in_3 <= '1' else "0000";
+	--------------------------------------------------------------------
 
 end std_nux1_arch;
